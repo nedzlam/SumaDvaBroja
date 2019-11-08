60f4461a494945dc2519f6879adecb10a8828d83
nedzlam <nedzlamemic@icloud.com>
2019-11-09 00:10:05 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a, b ;
+        Scanner unos = new Scanner (System.in);
+        System.out.println ("Unesi prvi broj a: ");
+        a = unos.nextInt();
+        System.out.println("Unesi drugi broj b: ");
+        b = unos.nextInt();
+        System.out.println("Zbir brojeva: " + (a + b));
+    }
+}
